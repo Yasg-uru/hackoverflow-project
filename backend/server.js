0e10bc335a6b678ca41e58b79251e7a3abc39856
yasg-uru <yashpawar12122004@gmail.com>
2024-03-11 16:28:25 +0530

    connecting database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectdatabase from "./config/database.js";
 dotenv.config({
   path: "backend/config/config.env",
 });
+connectdatabase();
 const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
   console.log(`server is running on port :${PORT}`);
